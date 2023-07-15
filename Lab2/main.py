3104a8793b8e9b217e41c53c2740aab38fc3a48c
JorgeMonterov <138807756+JorgeMonterov@users.noreply.github.com>
2023-07-14 23:16:23 -0600

    agrego mainpy

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,1 +1,14 @@
-from api import getOneUser
+from api import *
+from ids import elem
+import time
+
+usuarios = elem
+
+star_time=time.time()
+for user_id in usuarios:
+    
+    result = getOneUser(user_id)
+    print(result)
+
+duration=time.time()-star_time
+print(duration)
