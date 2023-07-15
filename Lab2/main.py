8563cd0cc040f2704f8dd80bd96329688f178aa5
JorgeMonterov <138807756+JorgeMonterov@users.noreply.github.com>
2023-07-14 23:21:29 -0600

    agrego api y ids

--- Lab2/main.py
+++ Lab2/main.py
@@ -6,9 +6,9 @@ usuarios = elem
 
 star_time=time.time()
 for user_id in usuarios:
-    
+    #llama la funcion y utilizando el diccionario de idds.
     result = getOneUser(user_id)
     print(result)
-
+#Formula para saber el tiempo que dura
 duration=time.time()-star_time
 print(duration)
